87b756cf74333f367bd4543ab15e255d193b7ec8
sunnythelucky <sunnyparkk@gmail.com>
2023-01-02 21:21:32 -0500

    Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./models";
 
 const App: React.FC = () => {
 	const [todo, setTodo] = useState<string>("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo("");
+		}
+	};
 
 	// useEffect(() => {
 	// 	console.log(todo);
-	// }, [todo]);
+	// console.log(todos)
+	// }, [todo, todos]);
 
 	return (
 		<div className="App">
 			<span className="heading">Taskify</span>
-			<InputField todo={todo} setTodo={setTodo} />
+			<InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+			<TodoList todos={todos} setTodos={setTodos} />
 		</div>
 	);
 };
---
+++ src/models.ts
@@ -0,0 +1,32 @@
+export interface Todo {
+	id: number;
+	todo: string;
+	isDone: boolean;
+}
+
+// import { useReducer } from "react";
+
+// type Actions =
+// 	| { type: "add"; payload: string }
+// 	| { type: "remove"; payload: number }
+// 	| { type: "done"; payload: number };
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+// 	switch (action.type) {
+// 		case "add":
+// 			return [...state, { id: Date.now(), todo: action.payload, isDone: false }];
+// 		case "remove":
+// 			return state.filter((todo) => todo.id !== action.payload);
+// 		case "done":
+// 			return state.map((todo) => {
+// 				todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo;
+// 			});
+// 		default:
+// 			break;
+// 	}
+// };
+
+// const ReducerExample = () => {
+// 	const [state, dispatch] = useReducer(TodoReducer, []);
+// 	return <div />;
+// };
